8b287fd0b49ad4e5fc817ff812fda07e9e296d1b
rifaset123 <rifaindra123456@gmail.com>
2024-11-25 15:57:25 +0700

    add layout themes

--- app/src/main/java/com/example/cimon_chilimonitoring/MainActivity.kt
+++ app/src/main/java/com/example/cimon_chilimonitoring/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.cimon_chilimonitoring
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.cimon_chilimonitoring.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.color.DynamicColors
+import com.google.android.material.elevation.SurfaceColors
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,5 +40,26 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // action bar color
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(getResources().getColor(R.color.white)))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_action_bar, menu)
+
+        val search = menu.findItem(R.id.menu_chat)
+        val searchView = search?.actionView as? SearchView
+
+        // disable search button in home fragment, settings, fav
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            // ngilangin search di fragment home, settings, fav
+            search?.isVisible = destination.id != R.id.navigation_home
+//                    && destination.id != R.id.navigation_settings
+//                    && destination.id != R.id.navigation_favorite
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/cimon_chilimonitoring/ui/notifications/TrackingFragment.kt
+++ app/src/main/java/com/example/cimon_chilimonitoring/ui/notifications/TrackingFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.cimon_chilimonitoring.databinding.FragmentNotificationsBinding
+import com.example.cimon_chilimonitoring.databinding.FragmentTrackingBinding
 
 class TrackingFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentTrackingBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +25,9 @@ class TrackingFragment : Fragment() {
         val trackingViewModel =
             ViewModelProvider(this).get(TrackingViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentTrackingBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        trackingViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
