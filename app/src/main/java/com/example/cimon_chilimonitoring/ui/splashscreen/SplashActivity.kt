76d01a07a5b09ad3ce868c41d4531e3fe5ca6c69
rifaset123 <rifaindra123456@gmail.com>
2024-12-02 21:43:02 +0700

    add intro and splash screen

---
+++ app/src/main/java/com/example/cimon_chilimonitoring/ui/splashscreen/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.cimon_chilimonitoring.ui.splashscreen
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.cimon_chilimonitoring.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, IntroActivity::class.java)
+            val options = ActivityOptions.makeSceneTransitionAnimation(this)
+            startActivity(intent, options.toBundle())
+            finish()
+        }, 5500)
+    }
+}
